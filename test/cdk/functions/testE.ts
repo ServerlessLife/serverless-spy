cf1cffd0a67856193d5ca2041996faac7d8249b4
Marko Strukelj <marko@serverlesslife.com>
2022-08-23 15:54:46 +0200

    fix test 2

--- test/cdk/functions/testE.ts
+++
@@ -1,9 +0,0 @@
-import { EventBridgeEvent } from 'aws-lambda';
-
-export const handler = async (event: EventBridgeEvent<any, any>) => {
-  console.log('RECEIVED EVENT:', JSON.stringify(event));
-
-  const message = event.detail;
-
-  return { message: 'Hello ' + message.key1 };
-};
--- test/cdk/functions/testSst2.ts
+++
@@ -1,5 +0,0 @@
-export const handler = async (event: any) => {
-  console.log('RECEIVED EVENT:', JSON.stringify(event));
-
-  return { message: 'Hello ' };
-};
